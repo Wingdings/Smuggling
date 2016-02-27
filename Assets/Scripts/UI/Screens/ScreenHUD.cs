9f70c2841fe4de9cca9b60d32edaf6cbcfaaab64
Robert Adams <robert.adams.iii@gmail.com>
2016-02-27 14:30:57 -0500

    Added group buttons
    added group buttons to hud

--- Assets/Scripts/UI/Screens/ScreenHUD.cs
+++ Assets/Scripts/UI/Screens/ScreenHUD.cs
@@ -7,15 +7,26 @@ public class ScreenHUD : ScreenBase {
     Text _moneyText;
     Text _reputationText;
 
+    Text _group1Count;
+    Text _group2Count;
+    Text _group3Count;
+
     // Use this for initialization
     public override void Start()
     {
         base.Start();
         getButtonByName("PauseButton").onClick.AddListener(delegate () { _ui.DoFlowEvent(FLOW_EVENT.FLOW_PAUSE_MENU_OPEN); });
         getButtonByName("SimulateButton").onClick.AddListener(delegate() { _ui.DoFlowEvent(FLOW_EVENT.FLOW_RESULTS_OPEN); });
+        getButtonByName("BorderGroupButton").onClick.AddListener(delegate () { _ui.DoFlowEvent(FLOW_EVENT.FLOW_RESULTS_OPEN); });
+        getButtonByName("BoatGroupButton").onClick.AddListener(delegate () { _ui.DoFlowEvent(FLOW_EVENT.FLOW_RESULTS_OPEN); });
+        getButtonByName("PlaneGroupButton").onClick.AddListener(delegate () { _ui.DoFlowEvent(FLOW_EVENT.FLOW_RESULTS_OPEN); });
+
 
         _moneyText = getTextByName("MoneyText");
         _reputationText = getTextByName("ReputationText");
+        _group1Count = getTextByName("BorderGroupText");
+        _group2Count = getTextByName("BoatGroupText");
+        _group3Count = getTextByName("PlaneGroupText");
     }
 
     // Update is called once per frame
@@ -24,6 +35,10 @@ public class ScreenHUD : ScreenBase {
         base.Update();
         _moneyText.text = "Funds: $" + _game.player.stats.money;
         _reputationText.text = "Reputation: " + _game.player.stats.reputation;
+
+        _group1Count.text = "" + _game.smugglingGroups[0].clients.Count;
+        _group2Count.text = "" + _game.smugglingGroups[1].clients.Count;
+        _group3Count.text = "" + _game.smugglingGroups[2].clients.Count;
     }
 
     public override void OpenScreen()
