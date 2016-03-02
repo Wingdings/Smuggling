72d7f8e6a533c6d8ed7745dfa58cd7a499f2e836
Robert Adams <robert.adams.iii@gmail.com>
2016-03-02 14:43:09 -0500

    Players lose rep for denying clients

--- Assets/Scripts/UI/Screens/ScreenDialog.cs
+++ Assets/Scripts/UI/Screens/ScreenDialog.cs
@@ -16,7 +16,7 @@ public class ScreenDialog : ScreenBase {
             _ui.DoFlowEvent(FLOW_EVENT.FLOW_GROUP_SELECT_OPEN); });
         getButtonByName("DenyButton").onClick.AddListener(delegate()
         {
-            //_game.player.changeReputation(-1 * _client.stats.desperation);
+            _game.player.changeReputation(-1 * (int)_client.stats.denyRep);
             _ui.DoFlowEvent(FLOW_EVENT.FLOW_DIALOG_CLOSE);
         });
 
